0bd5ee105f316c6acf283dd75c113553b57aebf3
Kirill Yurevich Ponomarev <drmckay-kirill@yandex.ru>
2020-10-06 21:35:43 +0500

    dll

---
+++ DllImportLab/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+using System;
+
+namespace DllImportLab
+{
+    class Program
+    {
+        private const string DllPath = "/home/drmckay/projects/dev_tech_labs/DllImportLab/Lib2-1.dll";
+
+        [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
+        public static extern double TheFunc(char[] name, double x);
+
+        static void Main(string[] args)
+        {
+            var value = TheFunc("Ponomarev".ToCharArray(), 0);
+        }
+    }
+}
