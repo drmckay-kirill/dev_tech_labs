a4eaafd7616070a80310ddec4a3c45bd5e03630c
Кирилл Пономарёв <PonomarevKY@it2g.ru>
2020-10-28 00:20:19 +0500

    ActiveX

---
+++ ActiveXDemoLib/ActiveXDemoLib/ActiveXDemoConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ActiveXDemoConsole
+{
+    class Program
+    {
+        private static string ProgId => "ActiveXDemoLib";
+
+        static void Main(string[] args)
+        {
+            var activeXLibType = Type.GetTypeFromProgID(ProgId)
+                ?? throw new ArgumentException($"Не удалось загрузить ActiveX объект с ProgId {ProgId}");
+            dynamic activeXObject = Activator.CreateInstance(activeXLibType);
+            
+            Console.WriteLine(activeXObject.FunctionName());
+            Console.WriteLine(activeXObject.MyFunctionValue(1));
+            Console.WriteLine(activeXObject.MyFunctionValue(2));
+        }
+    }
+}
---
+++ ActiveXDemoLib/ActiveXDemoLib/ActiveXDemoLib/MyFunction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ActiveXDemoLib
+{
+    // регистрация сборки
+    // C:\Windows\Microsoft.NET\Framework\v4.0.30319
+    // RegAsm.exe ActiveXDemoLib.dll
+    // RegAsm.exe ActiveXDemoLib.dll /u
+
+    [ProgId("ActiveXDemoLib")]
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [Guid("227C0604-65BC-4150-9B43-E52B04F6D377")]
+    [ComVisible(true)]
+    public class MyFunction
+    {
+        [ComVisible(true)]
+        public string FunctionName()
+        {
+            return "ActiveXDemoLib.Function";
+        }
+
+        [ComVisible(true)]
+        public double MyFunctionValue(double y)
+        {
+            return 0;
+        }
+    }
+}
