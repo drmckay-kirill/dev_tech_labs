bced0f1649fb2caf85414a22c18876dcb97200e1
Кирилл Пономарёв <PonomarevKY@it2g.ru>
2020-10-29 16:58:14 +0500

    com example

---
+++ ComDemoLib/ComDemoLib/ComDemoLib/ComDemoLibService.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ComDemoLib
+{
+    [Guid("94D7E3E6-1E2A-4B91-9C11-CB4BFE5C4CCD"), InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+    public interface IComDemoLibService
+    {
+        [DispId(1)]
+        string FuncName();
+
+        [DispId(2)]
+        double FuncValue(double x);
+    }
+
+    [Guid("46385161-4C92-4F59-9867-EA02227F9C51"), ClassInterface(ClassInterfaceType.AutoDispatch)]
+    public class ComDemoLibService : IComDemoLibService
+    {
+        public string FuncName()
+        {
+            return "ComDemoLibService";
+        }
+
+        public double FuncValue(double x)
+        {
+            return 65536;
+        }
+    }
+}
---
+++ ComDemoLib/ComDemoLib/ComDemoLibConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ComDemoLibConsole
+{
+    class Program
+    {
+        private static Guid TypeGuid => Guid.Parse("46385161-4C92-4F59-9867-EA02227F9C51");
+
+        static void Main(string[] args)
+        {
+            var objectType = Type.GetTypeFromCLSID(TypeGuid)
+                ?? throw new ArgumentException($"Сборка с идентификатором {TypeGuid} не обнаружена");
+            dynamic @object = Activator.CreateInstance(objectType);
+
+            Console.WriteLine(@object.FuncName());
+            Console.WriteLine(@object.FuncValue(1));
+            Console.WriteLine(@object.FuncValue(2));
+        }
+    }
+}
